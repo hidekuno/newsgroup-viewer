55060e02dcd4e27cbd280c48977335477c17cad1
hideki kuno <hidekuno@gmail.com>
2023-11-10 12:11:05 +0900

    pycodestyleのエラーに対応した

--- newsgroup.py
+++ newsgroup.py
@@ -3,18 +3,20 @@
 #
 #   This is python sample
 #   hidekuno@gmail.com
-import sys 
 import argparse
-import urllib.request
 import nntplib
+import sys
 import traceback
+import urllib.request
 from html.parser import HTMLParser
 
+
 def gmane():
-    s = nntplib.NNTP('news.gmane.io')
+    s = nntplib.NNTP("news.gmane.io")
     for n in s.list()[1]:
         print(n.group)
     s.quit()
+
 
 def fj():
     class MyHTMLParser(HTMLParser):
@@ -22,16 +24,19 @@ def fj():
             if len(attrs) > 0 and attrs[0][1][:5] == "news:":
                 print(attrs[0][1][5:])
 
-    u = urllib.request.urlopen('http://www.fj-news.org/active/fj-active-2006-07-20.html.en')
+    u = urllib.request.urlopen(
+        "http://www.fj-news.org/active/fj-active-2006-07-20.html.en"
+    )
     b = u.read()
     u.close()
     parser = MyHTMLParser()
-    parser.feed(b.decode('utf-8'))
+    parser.feed(b.decode("utf-8"))
+
 
 try:
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f','--fj',  action='store_true')
-    parser.add_argument('-g','--gmane',  action='store_true')
+    parser.add_argument("-f", "--fj", action="store_true")
+    parser.add_argument("-g", "--gmane", action="store_true")
     args = parser.parse_args(sys.argv[1:])
 
     if args.fj:
